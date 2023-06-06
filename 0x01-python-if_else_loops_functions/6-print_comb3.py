a0386e3b925f7fa3fa45ad3cc86fa77fe2e0ea0f
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-06-06 11:49:03 -0700

    print combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
-for i in range(0, 90):
-    if i % 10 > i / 10:
-        if i != 89:
-            print("{:02d}, ".format(i), end='')
-        else:
-            print("{:02d}".format(i))
-
-
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i+1, 10):
+        print("{:02d}".format(i*10 + j), end=', ' if i != 8 or j != 9 else '')
+print()
