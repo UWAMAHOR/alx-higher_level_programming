19b2a9df059d00072b135aad1903194d41dba0e0
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-06-08 07:18:02 -0700

    addition infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    len_args = len(sys.argv)
     sum = 0
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print("{}".format(sum))
+    int(sum)
+    if len_args == 1:
+        print("{}".format(sum))
+    else:
+        counter = 1
+        while counter < len_args:
+            sum += int(sys.argv[counter])
+            counter += 1
+        print("{}".format(sum))
+
 
