6ca34d42aa9815047c67bb0ac1b1efd0c5b2f6f6
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-06-14 11:47:07 -0700

    unique add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = set(my_list)
-    result = 0
-    for i in new_list:
-        result += i
-    return result
-
+    set_num = set(my_list)
+    return sum(set_num)
